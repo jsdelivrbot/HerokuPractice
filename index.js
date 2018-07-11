aa130ad3414170d8719552c6515ef078214fba25
sanjeevsharma1 <sanjeev.sharma@appirio.com>
2018-07-11 19:55:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 var pg = require('pg');
 
-var app = express()
-  app.use(express.static(path.join(__dirname, 'public')))
-  app.set('views', path.join(__dirname, 'views'))
-  app.set('view engine', 'ejs')
-  app.get('/', (req, res) => res.render('pages/index'))
+var app = express();
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+  app.get('/', (req, res) => res.render('pages/index'));
   app.post('/update',function(req,res){
     pg.connect(process.env.DATABASE_URL, function(err,conn,done){
     if(err) console.log(err);
